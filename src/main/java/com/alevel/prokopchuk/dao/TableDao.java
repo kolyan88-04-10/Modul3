535490441db89ae96489f7d52e9c6924cbabac30
Nikolay <NikolyanD@gmail.com>
2019-01-03 20:27:15 +0200

    111

--- src/main/java/com/alevel/prokopchuk/dao/TableDao.java
+++ src/main/java/com/alevel/prokopchuk/dao/TableDao.java
@@ -1,5 +1,31 @@
 package com.alevel.prokopchuk.dao;
 
-public class TableDao {
+import com.alevel.prokopchuk.models.Column;
+import com.alevel.prokopchuk.models.Node;
+import com.alevel.prokopchuk.models.Table;
 
+import java.util.Map;
+
+public class TableDao extends AbstractDao<Table> {
+    public static final String SQL_INSERT_INTO_TABLE = "INSERT INTO ";
+
+    @Override
+    public boolean create(Table model) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Table model) {
+        return false;
+    }
+
+    public boolean addNode(Table table, Node node) {
+        StringBuilder queryBuilder = new StringBuilder(SQL_INSERT_INTO_TABLE);
+        StringBuilder valuesBuilder = new StringBuilder();
+        for (Map.Entry<Column, String> entry : node.getValues().entrySet()) {
+            queryBuilder.append(entry.getKey().getName()).append(", ");
+            valuesBuilder.append(entry.getValue()).append(", ");
+        }
+        return false;
+    }
 }
